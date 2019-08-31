33e9f0bb912d1e7495d79960d6677993b28d5021
Lukasz <karolina.wojak@yahoo.com>
2019-08-31 17:10:56 +0200

    Adding and removing groceries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'grocerylist';
+  
+  groceryArray = [];
+
+  addGrocery(grocery: string){
+    this.groceryArray.push(grocery);
+  }
+
+  deleteGrocery(grocery: string) {
+    for (let i = 0; i <= this.groceryArray.length; i++) {
+      if (grocery == this.groceryArray[i]) {
+        this.groceryArray.splice(i,1);
+      }
+    }
+  }
+
+  grocerySubmit(grocery: string) {
+    console.log(grocery);
+    if (grocery != "") {
+      this.groceryArray.push(grocery);
+    }
+  }
 }
